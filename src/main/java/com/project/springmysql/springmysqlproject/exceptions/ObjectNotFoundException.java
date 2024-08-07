cec7c20d30530e61e9dcd7cdba625294d27f4ffc
Jose Lucio <jloliveirajunior1903@gmail.com>
2024-08-07 19:07:45 -0300

    ObjectNotFound Exception and StandardError implementation

---
+++ src/main/java/com/project/springmysql/springmysqlproject/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,13 @@
+package com.project.springmysql.springmysqlproject.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	public ObjectNotFoundException(String msg){
+		super(msg);
+	}
+	
+}
