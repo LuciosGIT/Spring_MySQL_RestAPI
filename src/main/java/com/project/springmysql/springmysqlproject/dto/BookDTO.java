66c4359b8465be12efa14b7d7431253e1319695c
Jose Lucio <jloliveirajunior1903@gmail.com>
2024-08-09 15:37:38 -0300

    Implementing BookDTO

---
+++ src/main/java/com/project/springmysql/springmysqlproject/dto/BookDTO.java
@@ -0,0 +1,62 @@
+package com.project.springmysql.springmysqlproject.dto;
+
+import java.util.Objects;
+
+public class BookDTO {
+	private Long id;
+	private String title;
+	private String author;
+	
+	public BookDTO() {
+		
+	}
+
+	public BookDTO(Long id, String title, String author) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.author = author;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BookDTO other = (BookDTO) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
