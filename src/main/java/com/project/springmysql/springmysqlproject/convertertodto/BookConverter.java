5ea24c6d7563549e50954f122eecc90312e867de
Jose Lucio <jloliveirajunior1903@gmail.com>
2024-08-09 15:38:17 -0300

    Implementing Book conversion to Dto

---
+++ src/main/java/com/project/springmysql/springmysqlproject/convertertodto/BookConverter.java
@@ -0,0 +1,34 @@
+package com.project.springmysql.springmysqlproject.convertertodto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
+
+import com.project.springmysql.springmysqlproject.domain.Books;
+import com.project.springmysql.springmysqlproject.dto.BookDTO;
+
+public class BookConverter {
+	
+	static ModelMapper mapper = new ModelMapper();
+	
+	
+	public static BookDTO convertBookToBookDTO(Books book) {
+		BookDTO bookDto = mapper.map(book, BookDTO.class);
+		return bookDto;
+	}
+	
+	public static List<BookDTO> convertListOfBooksToListOfBooksDTO(List<Books> books){
+		List<BookDTO> booksDto = new ArrayList<>();
+		for(Books book : books) {
+			booksDto.add(mapper.map(book, BookDTO.class));
+		}
+		return booksDto;
+		
+	}
+	
+	public static Books convertBookDTOtoBook(BookDTO bookDto) {
+		Books book = mapper.map(bookDto, Books.class);
+		return book;
+	}
+}
