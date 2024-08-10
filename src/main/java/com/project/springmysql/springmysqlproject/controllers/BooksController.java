45a827e872eccaad12a59a426c293203efbf8412
Jose Lucio <jloliveirajunior1903@gmail.com>
2024-08-10 14:48:15 -0300

    Updating controller

--- src/main/java/com/project/springmysql/springmysqlproject/controllers/BooksController.java
+++ src/main/java/com/project/springmysql/springmysqlproject/controllers/BooksController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -17,7 +18,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.project.springmysql.springmysqlproject.dto.BookDTO;
 import com.project.springmysql.springmysqlproject.services.BooksService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 
 @RestController
 @RequestMapping("/books")
