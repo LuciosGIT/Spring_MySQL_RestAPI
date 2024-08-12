347c86da4e8265ebc22fb8542576084c6a887800
LuciosGIT <jloliveirajunior1903@gmail.com>
2024-08-12 17:08:56 -0300

    CORS global implementation

--- src/main/java/com/project/springmysql/springmysqlproject/configuration/WebConfig.java
+++ src/main/java/com/project/springmysql/springmysqlproject/configuration/WebConfig.java
@@ -2,10 +2,12 @@ package com.project.springmysql.springmysqlproject.configuration;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import com.project.springmysql.springmysqlproject.serialization.converter.YamlJackson2HttpMessageConverter;
 
@@ -13,10 +15,25 @@ import com.project.springmysql.springmysqlproject.serialization.converter.YamlJa
 public class WebConfig implements WebMvcConfigurer {
 
 	private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
+
+	@Value("${cors.originPatterns:default}")
+	private String corsOriginPatterns = "";
+
 	@Override
 	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
 		converters.add(new YamlJackson2HttpMessageConverter());
 	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry corsRegistry){
+		var allowedOrigins = corsOriginPatterns.split(",");
+		corsRegistry.addMapping("/**")//.allowedMethods("GET","POST","PUT")
+				.allowedMethods("*")
+				.allowedOrigins(allowedOrigins)
+				.allowCredentials(true);
+
+	}
+
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
 		WebMvcConfigurer.super.configureContentNegotiation(configurer);
