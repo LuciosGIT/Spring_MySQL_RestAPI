2a7b0fe1abeb63a032b541af70cc2eb6823d7255
LuciosGIT <jloliveirajunior1903@gmail.com>
2024-08-14 12:20:11 -0300

    Implementing User repository and service

---
+++ src/main/java/com/project/springmysql/springmysqlproject/services/UserService.java
@@ -0,0 +1,38 @@
+package com.project.springmysql.springmysqlproject.services;
+
+import com.project.springmysql.springmysqlproject.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+
+
+
+@Service
+public class UserService implements UserDetailsService {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	public UserService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+
+
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		var user = userRepository.findByUsername(username);
+		if (user != null){
+			return user;
+		}
+		else {
+			throw new UsernameNotFoundException("Username" + username + "not found!");
+		}
+	}
+}
