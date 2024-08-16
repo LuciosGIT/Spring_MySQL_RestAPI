96f7858b682af31693cdb7252ee4d013dff93089
LuciosGIT <jloliveirajunior1903@gmail.com>
2024-08-15 23:23:31 -0300

    AuthenticationService implementation

---
+++ src/main/java/com/project/springmysql/springmysqlproject/services/AuthService.java
@@ -0,0 +1,50 @@
+package com.project.springmysql.springmysqlproject.services;
+
+import com.project.springmysql.springmysqlproject.dto.security.AccountCredentialsDTO;
+import com.project.springmysql.springmysqlproject.dto.security.TokenDTO;
+import com.project.springmysql.springmysqlproject.repositories.UserRepository;
+import com.project.springmysql.springmysqlproject.security.jwt.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserRepository repository;
+
+
+    @SuppressWarnings("rawtypes")
+    public ResponseEntity signIn(AccountCredentialsDTO data) {
+        try{
+            var userName = data.getUsername();
+            var password = data.getPassword();
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userName, password));
+
+            var user = repository.findByUsername(userName);
+
+            var tokenResponse = new TokenDTO();
+            if (user != null) {
+                tokenResponse = tokenProvider.createAccessToken(userName, user.getRoles());
+            }
+            else{
+                throw new UsernameNotFoundException("Username " + userName  + " not found!");
+            }
+            return ResponseEntity.ok(tokenResponse);
+        }
+        catch (Exception e){
+            throw new BadCredentialsException("Invalid username/password supplied!");
+        }
+    }
+}
