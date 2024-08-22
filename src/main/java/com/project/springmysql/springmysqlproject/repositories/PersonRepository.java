e0542c0c9706b4825fd59c00b5ba9fa1e02f1a81
LuciosGIT <jloliveirajunior1903@gmail.com>
2024-08-22 18:56:37 -0300

    Implementing JPQL query for disablePerson

--- src/main/java/com/project/springmysql/springmysqlproject/repositories/PersonRepository.java
+++ src/main/java/com/project/springmysql/springmysqlproject/repositories/PersonRepository.java
@@ -4,8 +4,17 @@ package com.project.springmysql.springmysqlproject.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.project.springmysql.springmysqlproject.domain.Person;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PersonRepository extends JpaRepository<Person,Long>{
 	boolean existsByEmail(String email);
     boolean existsByPhoneNumber(String phoneNumber);
+
+    @Query("UPDATE Person p p.enabled = false WHERE p.id =:id")
+
+    @Modifying
+    void disablePerson(@Param("id") Long id);
+
 }
