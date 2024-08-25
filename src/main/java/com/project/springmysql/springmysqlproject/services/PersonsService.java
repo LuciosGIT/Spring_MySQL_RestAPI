48fe9c181cac9c0fb02093238be105915ea4b85a
LuciosGIT <jloliveirajunior1903@gmail.com>
2024-08-24 23:05:50 -0300

    Implementing HATEOAS on pagination

--- src/main/java/com/project/springmysql/springmysqlproject/services/PersonsService.java
+++ src/main/java/com/project/springmysql/springmysqlproject/services/PersonsService.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.stereotype.Service;
 
 import com.project.springmysql.springmysqlproject.controllers.PersonController;
@@ -21,6 +24,9 @@ public class PersonsService {
 	
 	@Autowired
 	private PersonRepository userRepository;
+
+	@Autowired
+	private PagedResourcesAssembler<PersonDTO> assembler;
 	
 	public PersonDTO findById(Long id) {
 		PersonDTO user = PersonConverter.convertUserToUserDto(userRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found!")));
@@ -28,7 +34,7 @@ public class PersonsService {
 		return user;
 	}
 	
-	public Page<PersonDTO> findAll(Pageable pageable){
+	public PagedModel<EntityModel<PersonDTO>> findAll(Pageable pageable){
 
 		var personPage = userRepository.findAll(pageable);
 
@@ -36,7 +42,8 @@ public class PersonsService {
 
 		personDtosPage.map(p -> p.add(linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel()));
 
-		return personDtosPage;
+		Link link = linkTo(methodOn(PersonController.class).findAll(pageable.getPageNumber(), pageable.getPageSize(),"asc")).withSelfRel();
+		return assembler.toModel(personDtosPage, link);
 	}
 	
 	public PersonDTO create(PersonDTO obj) {
